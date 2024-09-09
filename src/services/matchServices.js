54aefe3d2886a41ea6553fc63baf8dc534ab23df
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2024-09-09 21:20:58 +0300

    fix date format in match cards

--- src/services/matchServices.js
+++ src/services/matchServices.js
@@ -10,7 +10,11 @@ export function matchesInfo(setMatches) {
 
             result.forEach(m => {
                 let [id, aTeamId, bTeamId, date, score] = m.split(',');
-                let formatedDate = new Date(date).toUTCString();
+                let formatedDate = new Date(date + 'Z').toUTCString();
+                // console.log(new Date(date).toDateString()); //Fri Jun 14 2024
+                // console.log(new Date(date));  //Fri Jun 14 2024 00:00:00 GMT+0300 (Eastern European Summer Time)
+                // console.log(new Date(date).toUTCString()); //Thu, 13 Jun 2024 21:00:00 GMT
+
 
                 setMatches((prevData => {
                     let result = [...prevData,
